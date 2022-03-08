f595d96daf2a834503ff9a1505d7a725636b65b4
Oscar Salazar <raczosala@yahoo.com>
2022-03-08 14:49:11 -0500

    fix: remove replace all with replace and regex

--- lib/snippets.ts
+++ lib/snippets.ts
@@ -2,7 +2,7 @@ import { Recipe } from "types/Recipe";
 
 export function getSnippetSlug(name: string | undefined) {
   if (name) {
-    return name.toLowerCase()?.replaceAll(" ", "-");
+    return name.toLowerCase()?.replace(/ /g, "-");
   }
 
   return "";
