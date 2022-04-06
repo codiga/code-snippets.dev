5e7792537a670bc7a90c65131ebc71bcce9b82fe
oscar <oscar@192.168.0.5>
2022-04-06 09:00:23 -0500

    feat: product hunt top daily post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/react";
 import HomeLayout from "components/layout/HomeLayout";
 import PageTitle from "components/shared/PageTitle";
 import SearchBox from "components/shared/search-box/SearchBox";
@@ -8,6 +9,21 @@ const Home: NextPageWithLayout = () => {
   return (
     <PageTitle title="Code Snippet Search">
       <SearchBox autoFocus />
+      <Box w="100%" my={8} textAlign="center">
+        <Box
+          as="a"
+          d="inline-block"
+          href="https://www.producthunt.com/posts/snipt-dev?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-snipt&#0045;dev"
+          target="_blank"
+        >
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=338490&theme=dark&period=daily"
+            alt="snipt&#0046;dev - Search&#0032;engine&#0032;for&#0032;code&#0032;snippets | Product Hunt"
+            width="250"
+            height="54"
+          />
+        </Box>
+      </Box>
     </PageTitle>
   );
 };
