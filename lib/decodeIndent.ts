6ecb65f0b6829b289778ce448f2da92c14156f09
Oscar Salazar <oscar@codiga.io>
2022-05-31 23:31:24 -0500

    feat: support utf-8 snippets

--- lib/decodeIndent.ts
+++ lib/decodeIndent.ts
@@ -1,7 +1,7 @@
 export function decodeIndent(code: string | undefined) {
   if (!code) return "";
 
-  const decodeFromBase64 = Buffer.from(code, "base64").toString("ascii");
+  const decodeFromBase64 = Buffer.from(code, "base64").toString("utf-8");
 
   return decodeFromBase64.replace(/&\[CODIGA_INDENT\]/g, "  ");
 }
